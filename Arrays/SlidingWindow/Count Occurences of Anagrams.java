6c21c5d1d54034c03bf8ce6cabb74e8cf2660df6
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-07-08 06:49:24 +0545

    Create Count Occurences of Anagrams.java

---
+++ Arrays/SlidingWindow/Count Occurences of Anagrams.java
@@ -0,0 +1,55 @@
+// Problem link : https://www.geeksforgeeks.org/problems/count-occurences-of-anagrams5839/1
+
+
+
+// User function Template for Java
+
+class Solution {
+    
+    //This is to check if the populated freq array is all 0 or not
+    private static boolean filled(int [] hehe){
+        for(int take : hehe){
+            if(take!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+
+    int search(String pat, String txt) {
+        
+        int [] freq = new int[26];
+        Arrays.fill(freq,0);
+    
+        
+        //populate the frequencey array
+        for( char ch : pat.toCharArray()){
+            
+            freq[ch-'a']++;
+        }
+        
+        int i= 0 , j = 0;
+        int n = txt.length();
+        int result = 0;
+        int ans = 0;
+        
+        while(j<n){
+            
+            freq[txt.charAt(j)-'a']--;
+            
+            //lets check if we got the window
+         if(j-i+1==pat.length()){
+             if(filled(freq)){
+                 ans++;
+             }
+             //slide the front pointer once window is found
+             freq[txt.charAt(i)-'a']++;
+             i++;
+         }
+            j++;
+            
+        }
+           return ans;        
+    }
+}
