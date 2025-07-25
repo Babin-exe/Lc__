759d678912c964c058cd829201a68e5c6282d2e7
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-07-25 09:56:09 +0545

    Update Binary Subarrays With Sum.java

--- Arrays/SlidingWindow/Binary Subarrays With Sum.java
+++ Arrays/SlidingWindow/Binary Subarrays With Sum.java
@@ -1,6 +1,6 @@
 // Problem link : https://leetcode.com/problems/binary-subarrays-with-sum/description/
 
-
+// Java:
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
 
@@ -32,3 +32,38 @@ class Solution {
         return sub;
     }
 }
+
+
+
+// c++ solution :
+// //////////////////////////////// //////////////////////////////// //////////////////////////////// //////////////////////////////// //
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int i = 0, j = 0;
+        int n = nums.size();
+        int sum = 0;
+        int sub = 0;
+        int zero_count = 0;
+        while (j < n) {
+            sum += nums[j];
+
+            while (i < j && ((sum > goal) || (nums[i] == 0))) {
+                sum -= nums[i];
+                if (nums[i] == 0) {
+                    zero_count = zero_count + 1;
+                } else {
+                    zero_count = 0;
+                }
+                i++;
+            }
+
+            if (sum == goal) {
+                sub = sub + 1 + zero_count;
+            }
+            j++;
+        }
+        return sub;
+    }
+};
