610fcf9c6ba914e55abf0f2b1b7d5c9954f9de54
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-08-01 20:21:56 +0545

    Create solution.cpp

---
+++ Arrays/ Maximize the Confusion of an Exam/solution.cpp
@@ -0,0 +1,34 @@
+// Problem link : https://leetcode.com/problems/maximize-the-confusion-of-an-exam/description/
+
+class Solution {
+
+private:
+    int blackBox(string s, int k, char c) {
+        int ans = 0, i = 0, j = 0, flips = 0;
+
+        while (j < s.length()) {
+
+            if (s[j] != c)
+                flips++;
+
+            while (flips > k) {
+
+                if (s[i] != c)
+                    flips--;
+
+                i++;
+            }
+
+            ans = max(ans, (j - i + 1));
+            j++;
+        }
+
+        return ans;
+    }
+
+public:
+    int maxConsecutiveAnswers(string answerKey, int k) {
+
+        return max(blackBox(answerKey, k, 'T'), blackBox(answerKey, k, 'F'));
+    }
+};
