61d494b17a8d1fc9f33a48bad7fca53c4634b11f
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-08-03 20:58:53 +0545

    Create Solution.cpp

---
+++ Arrays/BinarySearch/ Find Smallest Letter Greater Than Target/Solution.cpp
@@ -0,0 +1,19 @@
+// Problem link : https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
+
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int start = 0, end = letters.size() - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (letters[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.size()];
+    }
+};
