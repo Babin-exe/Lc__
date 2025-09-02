f6c621ba927c3f498db3643f49881f2b1ce8c2b1
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-09-02 22:16:44 +0545

    Create Solution.cpp

---
+++ Strings/Make The String Great/Solution.cpp
@@ -0,0 +1,17 @@
+// Problem link : https://leetcode.com/problems/make-the-string-great/
+class Solution {
+public:
+    string makeGood(string s) {
+
+        string ans = "";
+        for (char& ch : s) {
+            if (!ans.empty() &&
+                (ans.back() + 32 == ch || ans.back() - 32 == ch)) {
+                ans.pop_back();
+            } else {
+                ans.push_back(ch);
+            }
+        }
+        return ans;
+    }
+};
