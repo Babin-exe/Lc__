2d1e478be3311b2d75d3b3327d663a4cf983b517
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-09-13 09:42:20 +0545

    Update Solution.java

--- Strings/Repeated Substring Pattern/Solution.java
+++ Strings/Repeated Substring Pattern/Solution.java
@@ -21,3 +21,61 @@ class Solution {
         return false;
     }
 }
+
+// I don't know about these approaches 
+
+//Approach 1 : 
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string t = s + s;
+        return t.substr(1, t.size() - 2).find(s) != string::npos;
+    }
+};
+
+
+
+//Approach 2 : 
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.size();
+        vector<int> pi(n, 0);
+        
+        for (int i = 1, j = 0; i < n; i++) {
+            while (j > 0 && s[i] != s[j]) j = pi[j - 1];
+            if (s[i] == s[j]) j++;
+            pi[i] = j;
+        }
+
+        int lps = pi[n - 1]; // longest proper prefix also suffix
+        return lps > 0 && n % (n - lps) == 0;
+    }
+};
+
+//Approach 3 : 
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i != 0) continue;
+
+            string sub = s.substr(0, i);
+            string ans;
+            ans.reserve(n); // avoid reallocation
+
+            for (int j = 0; j < n / i; j++) {
+                ans.append(sub);
+            }
+
+            if (ans == s) return true;
+        }
+        return false;
+    }
+};
+
+
+
+
+
