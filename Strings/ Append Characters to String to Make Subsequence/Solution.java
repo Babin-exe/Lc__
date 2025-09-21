faa70fdd695ca1ab3a1b0049a2c5cfb0ca444967
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-09-22 00:19:24 +0545

    Create Solution.java

---
+++ Strings/ Append Characters to String to Make Subsequence/Solution.java
@@ -0,0 +1,18 @@
+// Problem link : https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/
+class Solution {
+    public int appendCharacters(String s, String t) {
+
+        int m = s.length();
+        int n = t.length();
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                i++;
+            }
+        }
+        return n - j;
+    }
+}
