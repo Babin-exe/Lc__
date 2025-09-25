dd0f589b7ca643f67ac28d5002d807f640307c29
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-09-25 14:50:54 +0545

    Create Solution.cpp

---
+++ Strings/Count Beautiful Substrings I/Solution.cpp
@@ -0,0 +1,38 @@
+// Problem link : https://leetcode.com/problems/count-beautiful-substrings-i/submissions/1782134308/
+
+class Solution {
+
+public:
+    bool isVowel(char ch) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            return true;
+        return false;
+    }
+
+public:
+#define ll long long
+    long long beautifulSubstrings(string s, int k) {
+        ll result = 0;
+        ll vowel = 0;
+        ll consonant = 0;
+        unordered_map<ll, unordered_map<ll, ll>> mp;
+        mp[0][0]++;
+
+        for (char& ch : s) {
+
+            if (isVowel(ch))
+                vowel++;
+            else
+                consonant++;
+
+            ll ps = vowel - consonant;
+
+            for (auto& [pastVowel, count] : mp[ps]) {
+                if ((vowel - pastVowel) * (vowel - pastVowel) % k == 0)
+                    result += count;
+            }
+            mp[ps][vowel]++;
+        }
+        return result;
+    }
+};
