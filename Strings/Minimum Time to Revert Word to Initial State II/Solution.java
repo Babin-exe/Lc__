460c04ac79618430eae436181f78637e77150a84
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-10-01 19:25:00 +0545

    Create Solution.java

---
+++ Strings/Minimum Time to Revert Word to Initial State II/Solution.java
@@ -0,0 +1,47 @@
+// Problem link : https://leetcode.com/problems/minimum-time-to-revert-word-to-initial-state-ii/description/
+class Solution {
+
+    public void fillLPS(String word, int[] LPS, int n) {
+
+        LPS[0] = 0;
+        int i = 1;
+        int length = 0;
+        while (i < n) {
+            if (word.charAt(i) == word.charAt(length)) {
+                length++;
+                LPS[i] = length;
+                i++;
+            } else {
+                if (length == 0) {
+                    LPS[i] = 0;
+                    i++;
+                } else {
+
+                    length = LPS[length - 1];
+
+                }
+
+            }
+        }
+
+    }
+
+    public int minimumTimeToInitialState(String word, int k) {
+
+        int n = word.length();
+        int[] LPS = new int[n];
+        fillLPS(word, LPS, n);
+
+        int value = LPS[n - 1];
+
+        while (value > 0 && (n - value) % k != 0) {
+
+            value = LPS[value - 1];
+
+        }
+        if (value != 0) {
+            return ((n - value) / k);
+        }
+        return (int) Math.ceil(n / (double) k);
+    }
+}
