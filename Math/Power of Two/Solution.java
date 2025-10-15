5945712dc84d88ddf28abbd58bc08db78623933b
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-10-15 16:14:32 +0545

    Create Solution.java

---
+++ Math/Power of Two/Solution.java
@@ -0,0 +1,10 @@
+// Problem Link : https://leetcode.com/problems/power-of-two/description/
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0)
+            return false;
+        if (n < 0)
+            return false;
+        return (n & (n - 1)) == 0;
+    }
+}
