e9b5036c29b6128aed327ec94de346eeaee86a20
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-10-21 12:08:19 +0545

    Create Solutin.java

---
+++ Recursion/subsets/Solutin.java
@@ -0,0 +1,23 @@
+// Problem Link : https://leetcode.com/problems/subsets/description/
+
+
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public void solve(int i, int [] nums, List<Integer> temp) {
+        if (i >= nums.length) {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        temp.add(nums[i]);
+        solve(i + 1, nums, temp);
+        temp.remove(temp.size() - 1);
+        solve(i + 1, nums, temp);
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> temp = new ArrayList<>();
+        solve(0, nums, temp);
+        return ans;
+    }
+}
