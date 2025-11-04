df6a2e42cba1e92246306966abbea552cf37ae69
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-11-04 22:02:09 +0545

    Create Solution.cpp

---
+++ Arrays/Maximum Product of Three Elements After One Replacement/Solution.cpp
@@ -0,0 +1,18 @@
+// Problem Link : https://leetcode.com/problems/maximum-product-of-three-elements-after-one-replacement/description/
+
+class Solution {
+public:
+    long long maxProduct(vector<int>& nums) {
+        long long a = LONG_MIN, b = LONG_MIN;
+        for (long xx : nums) {
+            long x = llabs(xx);
+            if (x > a) {
+                b = a;
+                a = x;
+            } else if (b < x) {
+                b = x;
+            }
+        }
+        return a * b * 1e5;
+    }
+};
