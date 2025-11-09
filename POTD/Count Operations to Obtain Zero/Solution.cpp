3af97755d0a8825f832ae80877e38697c4bd8fce
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-11-09 17:29:14 +0545

    Create Solution.cpp

---
+++ POTD/Count Operations to Obtain Zero/Solution.cpp
@@ -0,0 +1,25 @@
+// Problem Link : https://leetcode.com/problems/count-operations-to-obtain-zero/
+
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+
+        if (num1 == 0 || num2 == 0)
+            return 0;
+        int count = 0;
+        while (true) {
+            if (num1 >= num2) {
+                num1 -= num2;
+                count++;
+                if (num1 == 0)
+                    break;
+            } else {
+                num2 -= num1;
+                count++;
+                if (num2 == 0)
+                    break;
+            }
+        }
+        return count;
+    }
+};
