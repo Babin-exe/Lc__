5f9a949aeca55d579ed668db519b824697c76a74
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-11-11 00:04:35 +0545

    Create Solution.java

---
+++ Contest/Count Subarrays With Majority Element I/Solution.java
@@ -0,0 +1,19 @@
+// Problem Link : https://leetcode.com/problems/count-subarrays-with-majority-element-i/description/
+class Solution {
+    public int countMajoritySubarrays(int[] nums, int target) {
+
+           int ans = 0 ;
+        for(int i = 0 ;i < nums.length;i++){
+            int t_count = 0;
+           int idx = i;
+            for(int j = i;j<nums.length;j++){
+                idx = (j-i +1) / 2;
+                if(nums[j]==target)t_count++;
+
+                if(t_count > idx) ans++;
+
+            }
+        }
+        return ans;
+    }
+}
