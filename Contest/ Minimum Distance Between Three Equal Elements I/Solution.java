8ef3e5177f25e8916937aa61ed204a7aae6e4dc5
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-11-12 16:55:24 +0545

    Create Solution.java

---
+++ Contest/ Minimum Distance Between Three Equal Elements I/Solution.java
@@ -0,0 +1,18 @@
+// Problem Link : https://leetcode.com/problems/minimum-distance-between-three-equal-elements-i/description/
+
+class Solution {
+    public int minimumDistance(int[] nums) {
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] == nums[j] && nums[j] == nums[k]) {
+                        ans = Math.min(ans, 2 * (k - i));
+                    }
+
+                }
+            }
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
