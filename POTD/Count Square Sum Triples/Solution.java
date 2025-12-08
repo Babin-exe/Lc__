2791a5af2bdc4771d3bf20e033043d90fd3023f1
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-12-08 16:36:28 +0545

    Implement countTriples method for square sum triples

---
+++ POTD/Count Square Sum Triples/Solution.java
@@ -0,0 +1,16 @@
+// Problem Link : https://leetcode.com/problems/count-square-sum-triples
+
+class Solution {
+    public int countTriples(int n) {
+        int count = 0;
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= n; b++) {
+                int c = (int) Math.sqrt(a * a + b * b);
+                if (c * c == (a * a + b * b) && c <= n) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
