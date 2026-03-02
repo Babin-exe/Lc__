1878ae20ef699267af60b707d2d5c3684a8b9ee0
Babin <155212596+Babin-exe@users.noreply.github.com>
2026-03-02 11:33:04 +0545

    Create Solution.cpp

---
+++ Contest/Weekly Contest 491/Minimum Cost to Split into Ones/Solution.cpp
@@ -0,0 +1,9 @@
+// Problem Link: https://leetcode.com/problems/minimum-cost-to-split-into-ones/description/
+class Solution {
+public:
+    int minCost(int n) {
+        return (n * (n - 1)) / 2;
+    }
+};
+
+
