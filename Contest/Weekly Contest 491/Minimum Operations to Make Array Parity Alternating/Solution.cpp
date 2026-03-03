76b1078a5224defe27837c55bd1be111ef37e843
Babin <155212596+Babin-exe@users.noreply.github.com>
2026-03-03 10:56:30 +0545

    Create Solution.cpp

---
+++ Contest/Weekly Contest 491/Minimum Operations to Make Array Parity Alternating/Solution.cpp
@@ -0,0 +1,26 @@
+// Problem Link : https://leetcode.com/problems/minimum-operations-to-make-array-parity-alternating/description/
+
+/*
+Though process
+
+An array can either go with the sequence [odd,even,odd,even,odd ,even .......]
+or [even, odd , even , odd , even , odd , .......]
+
+we can simply try two  iterations and see for both sequences and store how many
+changes will it require for both of these cases and the minimum of those gives
+us the answer for our 1st case
+
+we were just concerned about changing the parity there was no record of either
++1 was done or -1 was done to change the partiy so we somehow need to know all
+the values  at that index +-1 and check by taking both of them in our final
+array and see that the maxium of the array - mininum of the array is minimized
+
+I still have not figured out how woudl i tackle the 2nd case lets seeee....
+
+
+*/
+
+class Solution {
+public:
+    vector<int> makeParityAlternating(vector<int>& nums) {}
+};
