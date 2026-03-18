92b7f24f010186b7e1af8806e872d934c55f1c74
Babin <155212596+Babin-exe@users.noreply.github.com>
2026-03-18 21:36:04 +0545

    Create Solution.cpp

---
+++ Contest/Weekly Contest 493/Count Commas in Range/Solution.cpp
@@ -0,0 +1,12 @@
+// Problem Link : https://leetcode.com/problems/count-commas-in-range
+
+
+class Solution {
+public:
+    int countCommas(int n) {
+        if (n < 999)
+            return 0;
+
+        return n - 999;
+    }
+};
