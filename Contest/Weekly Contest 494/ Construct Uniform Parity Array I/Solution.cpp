8a286c1fd596d47df60fc005cfd780d14502759c
BINAYA NEPAL <155212596+Babin-exe@users.noreply.github.com>
2026-03-24 06:10:19 +0545

    Create Solution.cpp

---
+++ Contest/Weekly Contest 494/ Construct Uniform Parity Array I/Solution.cpp
@@ -0,0 +1,17 @@
+// Problem Link : https://leetcode.com/problems/construct-uniform-parity-array-i/submissions/1956250137/
+/*
+All odds , even - true it is 
+
+If at least one odd that can make all evens to odd fine again
+
+If abscence of odd results in all even again fine 
+
+we can tell always true
+
+ */
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
