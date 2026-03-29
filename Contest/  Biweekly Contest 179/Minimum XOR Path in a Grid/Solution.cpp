42d18ec22fef74e9f0ff93d11f30c49d75cbf9b7
Baaabin <155212596+Babin-exe@users.noreply.github.com>
2026-03-29 20:44:50 +0545

    Update Solution.cpp

--- Contest/  Biweekly Contest 179/Minimum XOR Path in a Grid/Solution.cpp
+++ Contest/  Biweekly Contest 179/Minimum XOR Path in a Grid/Solution.cpp
@@ -27,3 +27,44 @@
 //         return *dp[m - 1][n - 1].begin();
 //     }
 // };
+
+
+class Solution {
+public:
+    int minCost(vector<vector<int>>& grid) {
+        
+        int m = grid.size(), n = grid[0].size();
+
+        vector<vector<bitset<1024>>> dp(m, vector<bitset<1024>>(n));
+
+        dp[0][0].set(grid[0][0]);
+
+
+        for (int j = 1; j < n; j++)
+            for (int v = 0; v < 1024; v++)
+                if (dp[0][j - 1].test(v))
+                    dp[0][j].set(v ^ grid[0][j]);
+
+
+        for (int i = 1; i < m; i++)
+            for (int v = 0; v < 1024; v++)
+                if (dp[i - 1][0].test(v))
+                    dp[i][0].set(v ^ grid[i][0]);
+
+
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                for (int v = 0; v < 1024; v++) {
+                    if (dp[i - 1][j].test(v)) 
+                        dp[i][j].set(v ^ grid[i][j]);
+                    if (dp[i][j - 1].test(v)) 
+                        dp[i][j].set(v ^ grid[i][j]);
+                }
+
+        for (int v = 0; v < 1024; v++)
+            if (dp[m - 1][n - 1].test(v))
+                return v;
+
+        return -1;
+    }
+};
