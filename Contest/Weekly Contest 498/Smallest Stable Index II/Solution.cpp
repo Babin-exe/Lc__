8c0bc2361c560635cebbb6c8123c599871d73a5c
Baaabin <155212596+Babin-exe@users.noreply.github.com>
2026-04-25 12:59:25 +0545

    Create Solution.cpp

---
+++ Contest/Weekly Contest 498/Smallest Stable Index II/Solution.cpp
@@ -0,0 +1,38 @@
+// Probelm Link : https://leetcode.com/problems/smallest-stable-index-ii/description/
+
+
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+
+        int n = nums.size();
+
+        vector<int> maxis(n);
+        vector<int> minis(n);
+
+        maxis[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            maxis[i] = max(maxis[i - 1], nums[i]);
+        }
+
+        minis[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            minis[i] = min(minis[i + 1], nums[i]);
+        }
+
+        int ans = 0;
+
+
+        for (int i = 0; i < n; i++) {
+            int mx = maxis[i];
+            int mi = minis[i];
+
+            if ((mx - mi) <= k)
+                return i;
+        }
+
+        return -1;
+    }
+};
