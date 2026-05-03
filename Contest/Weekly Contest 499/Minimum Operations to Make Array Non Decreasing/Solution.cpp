f9c95cf76541ae45ea238cf1fb061be9f5aa5cc5
Baaabin <155212596+Babin-exe@users.noreply.github.com>
2026-05-03 19:14:37 +0545

    Add solution for minimum operations to non-decreasing array
    Implement a solution to calculate the minimum number of operations required to make the array non-decreasing.

---
+++ Contest/Weekly Contest 499/Minimum Operations to Make Array Non Decreasing/Solution.cpp
@@ -0,0 +1,14 @@
+// Problem Link : https://leetcode.com/problems/minimum-operations-to-make-array-non-decreasing/description/
+
+class Solution {
+public:
+    long long minOperations(vector<int>& nums) {
+        long long ans = 0;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                ans += (nums[i] - nums[i + 1]);
+            }
+        }
+        return ans;
+    }
+};
