ad5cbd5552fe36114507e287efed0b7039954421
Baaabin <155212596+Babin-exe@users.noreply.github.com>
2026-05-03 12:51:17 +0545

    Add dice combinations solution in Solution.cpp
    Implement dynamic programming solution for dice combinations problem.

---
+++ DP/Dice Combinations/Solution.cpp
@@ -0,0 +1,27 @@
+// Problem Link : https://cses.fi/problemset/result/17066248/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int mod = 1e9 + 7;
+
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= 6; j++)
+        {
+            if (i - j >= 0)
+            {
+                dp[i] = (dp[i] + dp[i - j]) % mod;
+            }
+        }
+    }
+
+    cout << dp[n] << endl;
+}
