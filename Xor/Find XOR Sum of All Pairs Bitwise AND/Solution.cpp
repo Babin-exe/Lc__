b24cab0654969955545b053d8ac1a37f6bb24b9f
Baaabin <155212596+Babin-exe@users.noreply.github.com>
2026-07-24 20:03:00 +0545

    Create Solution.cpp

---
+++ Xor/Find XOR Sum of All Pairs Bitwise AND/Solution.cpp
@@ -0,0 +1,12 @@
+// Problem Link : https://leetcode.com/problems/find-xor-sum-of-all-pairs-bitwise-and/description/
+class Solution {
+public:
+    int getXORSum(vector<int>& arr1, vector<int>& arr2) {
+        int a = 0, b = 0;
+        for (auto& it : arr1)
+            a ^= it;
+        for (auto& it : arr2)
+            b ^= it;
+        return a & b;
+    }
+};
